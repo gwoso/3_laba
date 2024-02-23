97c6733c4f8102e85b14c43f011b38a0bfbb69b2
Ksenia <150545779+gwoso@users.noreply.github.com>
2024-02-24 01:32:20 +0500

    Update 2_recursion.py

--- 2_recursion.py
+++ 2_recursion.py
@@ -1,15 +1,3 @@
-def b_k(b, x, k): 
-    if k == 0:
-        return b
-    bk = b * (x ** 2)
-    return bk
-
-def y_k(b, y, k): 
-    if k == 0:
-        return b
-    yk = b * y
-    return yk
-
 def func(x, k): 
     if x <= 0:
         print("Incorrect value!")
@@ -20,10 +8,12 @@ def func(x, k):
     def f(b0, y0, k):
         if k == 0:
             return y0
-        bk = b_k(b0, x, k)
+        bk = b0 * (x ** 2)
         b0 = bk
-        yk = y_k(bk, y0, k)
+
+        yk = b0 * y0
         y0 = yk
+
         k = k - 1
         return f(b0, y0, k)
     return f(b0, y0, k)
