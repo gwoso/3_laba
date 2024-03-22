40f894847c0ca9c6f3b8ca5e254c0df0716120fb
Ksenia <150545779+gwoso@users.noreply.github.com>
2024-03-23 00:15:41 +0500

    Update 1_no_rec.py

--- 1_no_rec.py
+++ 1_no_rec.py
@@ -4,6 +4,24 @@ def create_n_dim_array(n, x):
         biba = [biba.copy() for _ in range(x)]
     return biba
 
-result = create_n_dim_array(2, 3)
-for row in result:
-    print(row)
+def printf(n, result):
+    if n == 2:
+        print("[")
+        for a in result:
+            print("\t", a)
+        print("]")
+
+    if n == 3:
+        print("[")
+        for a in result:
+            print("\t [")
+            for b in a:
+                print("\t\t", b)
+            print("\t ]")
+        print("]")
+
+n = int(input("Enter n: "))
+x = int(input("Enter x: "))
+
+result = create_n_dim_array(n, x)
+printf(n, result)
