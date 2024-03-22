680556c5173eba0475fbe990f30a861ef6e0c24d
Ksenia <150545779+gwoso@users.noreply.github.com>
2024-03-23 00:16:21 +0500

    Update 1_recursion.py

--- 1_recursion.py
+++ 1_recursion.py
@@ -3,7 +3,24 @@ def create_n_dim_array(n, x, level = 1):
         return [f'level {level}']*x
     return [(create_n_dim_array(n-1, x, level + 1))] * x
 
+def printf(n, result):
+    if n == 2:
+        print("[")
+        for a in result:
+            print("\t", a)
+        print("]")
 
-result = create_n_dim_array(2, 3)
-for row in result:
-    print(row)
+    if n == 3:
+        print("[")
+        for a in result:
+            print("\t [")
+            for b in a:
+                print("\t\t", b)
+            print("\t ]")
+        print("]")
+
+n = int(input("Enter n: "))
+x = int(input("Enter x: "))
+
+result = create_n_dim_array(n, x)
+printf(n, result)
